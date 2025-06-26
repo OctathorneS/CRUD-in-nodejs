3ba051d416f5dc80bf83523dc114586dfc1beb04
Amman Sajjad <ammansajjad42@gmail.com>
2025-06-26 16:01:06 +0500

    update 4pm

--- models/index.js
+++ models/index.js
@@ -1,10 +1,13 @@
 const sequelize = require('../config/db');
 const User = require('./user');
 const Task = require('./task');
-
+const Category = require('./category')
 // Associations
 User.hasMany(Task, { foreignKey: 'userId' });
 Task.belongsTo(User, { foreignKey: 'userId' });
+
+Task.belongsToMany(Category,{through:'TaskCategories',foreignKey:'taskId'});
+Category.belongsToMany(Task,{through:'TaskCategories',foreignKey:'categoryId'});
 
 // Scope definition after associations
 User.addScope('withTasks', {
